2409d89f028751942c41c2b034f6858c4652d421
Jhialilata Dash <jhialilatadash@gmail.com>
2025-11-13 21:56:57 +0530

    delete javascript

--- javascript.js
+++ javascript.js
@@ -10,8 +10,4 @@ let checkkk= function(name){
     console.log("take my name", name)
 }
 checkkk("lata");
-checkkk("lata");
-let checkkk= function(name){
-    console.log("take my name", name)
-}
-checkkkk("lata");
+
